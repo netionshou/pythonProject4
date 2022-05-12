345c615ddbc1bb55e4411a92c9bdcdeaeeb95883
Falshunova Nastya 1-МД-4 <99950967+netionshou@users.noreply.github.com>
2022-05-13 00:31:08 +0300

    Add files via upload

---
+++ maiiim.py
@@ -0,0 +1,86 @@
+import requests
+import telebot
+from telebot import types
+f = open('data/facts.txt', 'r', encoding='UTF-8')
+facts = f.read().split('\n')
+f.close()
+f = open('data/thinks.txt', 'r', encoding='UTF-8')
+thinks  = f.read().split('\n')
+f.close()
+
+bot = telebot.TeleBot('5295187259:AAH-CcDL0jV841-UBkfgvXQbEkkF-uVT16I')
+@bot.message_handler(commands=["start"])
+def start(message, res=False):
+    chat_id = message.chat.id
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Главное меню")
+    btn2 = types.KeyboardButton("❓ Помощь")
+    markup.add(btn1, btn2)
+
+    bot.send_message(chat_id,
+                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                         message.from_user), reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    chat_id = message.chat.id
+    ms_text = message.text
+
+    if ms_text == "Главное меню" or ms_text == "👋 Главное меню" or ms_text == "Вернуться в главное меню":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Развлечения")
+        btn2 = types.KeyboardButton("Управление")
+        back = types.KeyboardButton("Помощь")
+        markup.add(btn1, btn2, back)
+        bot.send_message(chat_id, text="Вы в главном меню", reply_markup=markup)
+
+    elif ms_text == "Развлечения":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Прислать собаку")
+        btn2 = types.KeyboardButton("Прислать анекдот")
+        btn3 = types.KeyboardButton("Факт")
+        btn4 = types.KeyboardButton("Поговорка")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, btn3, btn4,  back)
+        bot.send_message(chat_id, text="Развлечения", reply_markup=markup)
+
+    elif ms_text == "/dog" or ms_text == "Прислать собаку":
+        contents = requests.get('https://random.dog/woof.json').json()
+        urlDOG = contents ['url']
+        bot.send_photo(chat_id, photo = urlDOG, caption = "Лови собачку!")
+    elif ms_text == "Прислать анекдот":
+        def get_anekdot():
+            array_anekdots = []
+            req_anek = requests.get('http://anekdotme.ru/random')
+            soup = bs4.BeautifulSoup(req_anek.text, "html.parser")
+            result_find = soup.select('.anekdot_text')
+            for result in result_find:
+                array_anekdots.append(result.getText().strip())
+            return array_anekdots[0]
+
+    bot.polling(none_stop=True, interval=0)
+    elif ms_text == "Факт":
+        if message.text.strip() == 'Факт':
+            answer = random.choice(facts)
+        bot.send_message(message.chat.id, answer)
+    elif ms_text == "Поговорка":
+       if message.text.strip() == 'Поговорка':
+            answer = random.choice(thinks)
+        bot.send_message(message.chat.id, answer)
+    elif ms_text == "Управление":
+        bot.send_message(chat_id, text="еще не готово...")
+
+    elif ms_text == "Помощь" or ms_text == "/help":
+        bot.send_message(chat_id, "Автор: Швец Андрей")
+        key1 = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text="Напишите автору", url="https://t.me/user59387")
+        key1.add(btn1)
+        img = open('Швец Андрей.png', 'rb')
+        bot.send_photo(message.chat.id, img, reply_markup=key1)
+
+    else:
+        bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
+
+bot.polling(none_stop=True, interval=0)
