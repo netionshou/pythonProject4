15de98d9bb42437475fade8f989530d943a692af
Falshunova Nastya 1-МД-4 <99950967+netionshou@users.noreply.github.com>
2022-05-13 10:37:02 +0300

    Update mainppl.py

--- mainppl.py
+++ mainppl.py
@@ -3,6 +3,12 @@ import requests
 import telebot
 import random
 from telebot import types
+f = open('data/facts.txt', 'r', encoding='UTF-8')
+facts = f.read().split('\n')
+f.close()
+f = open('data/thinks.txt', 'r', encoding='UTF-8')
+thinks  = f.read().split('\n')
+f.close()
 
 bot = telebot.TeleBot('5295187259:AAH-CcDL0jV841-UBkfgvXQbEkkF-uVT16I')
 
